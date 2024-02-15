7f2bf576c5d6ff1f69ef48ff28bcb7c8d06647b4
hayatfaily <haya.toufayli.2020@outlook.com>
2024-02-16 00:06:07 +0200

    mmmmmmmmmmmmmmmmmmm

--- assets/js/movieSingle.js
+++ assets/js/movieSingle.js
@@ -1,7 +1,9 @@
-$("#times li").click(function () {
-    $("#times li").removeClass("selected");
-    $(this).addClass("selected");
-  });
+$("#times").on("click", "li", function () {
+ 
+  $("#times li").removeClass("selected");
+  
+  $(this).addClass("selected");
+});
 
 function clearError() {
     $("#errorday").text("");
@@ -13,46 +15,3 @@ function clearError() {
   $("#nbpeople").on("input", clearError);
   var name=document.getElementById("name");
 
-  $("#bookform").submit(function (e) {
-    e.preventDefault();
-    
-    let nbpeople = $("#nbpeople").val();
-    let selectedTime = $("#times li.selected").text();
-    let selectedDay = $("#selectDay").val();
-    console.log(selectedDay)
-    console.log(selectedTime)
-
-    if (nbpeople < 5 && nbpeople!=="" && selectedDay !== "" && selectedTime !== "") {
-        $.ajax({
-            method:"POST",
-            url:"functions/addTicket.php",
-             cache: false,
-            data:{
-                name:name,
-                nbpeople:nbpeople,
-                time:selectedTime,
-                day:selectedDay,
-            },
-            success:function(response){
-                alert("successssssssssss");
-                console.log(response);
-            }
-           })
-          
-    } else {
-        if (nbpeople > 5) {
-            $("#errornb").html('<i class="fa-solid fa-circle-exclamation"></i> You should enter a number < 5');
-          }
-          if (nbpeople ==="") {
-            $("#errornb").html('<i class="fa-solid fa-circle-exclamation"></i> This field is required < 5');
-          }
-
-          if (!selectedTime) {
-            $("#errortime").html('<i class="fa-solid fa-circle-exclamation"></i> You should select a time');
-          }
-
-          if (selectedDay === "") {
-            $("#errorday").html('<i class="fa-solid fa-circle-exclamation"></i> This field is required');
-          }
-    }
-  });
